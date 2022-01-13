c927a2b1e2f10326d53e28b295ddb8032933597c
Rafael Ramirez <rafa.yael@gmail.com>
2022-01-13 15:45:57 -0600

    feat: footer template agregado

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component'
 
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule, RouterModule,],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
