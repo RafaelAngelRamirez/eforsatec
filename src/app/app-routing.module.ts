d4609d9a2b53913a814b1756187ae722bd3f7d35
Rafael Ramirez <rafa.yael@gmail.com>
2022-01-13 15:08:49 -0600

    feat: Estructura básica de navegación creada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'nosotros',
+    loadChildren: () =>
+      import('./nosotros/nosotros.module').then((m) => m.NosotrosModule),
+  },
+  {
+    path: 'contacto',
+    loadChildren: () =>
+      import('./contacto/contacto.module').then((m) => m.ContactoModule),
+  },
+  {
+    path: 'productos',
+    loadChildren: () =>
+      import('./productos/productos.module').then((m) => m.ProductosModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
